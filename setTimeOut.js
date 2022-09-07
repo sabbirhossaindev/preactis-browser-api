87c3d24356135a40b4cc359193363b190f351b50
sabbir hossain <mdsabbir477470@gmail.com>
2022-09-07 17:24:05 +0600

    set time out done

---
+++ setTimeOut.js
@@ -0,0 +1,13 @@
+// synchronous example...
+
+console.log('I');
+console.log('am');
+console.log('sabbir');
+
+// Asynchronous explore...
+
+console.log('I');
+setTimeout(() => {
+    console.log('am');
+}, 3500);
+console.log('sabbir');
