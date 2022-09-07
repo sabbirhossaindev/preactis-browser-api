ea71a749aab082d10aa668c0fcf3c1208623516f
sabbir hossain <mdsabbir477470@gmail.com>
2022-09-07 19:07:05 +0600

    cookies

---
+++ cookies.js
@@ -0,0 +1,4 @@
+
+const cookies = () => {
+    alert('A cookie is an amount of information that persists between a server-side and a client-side. A web browser stores this information at the time of browsing. A cookie contains the information as a string generally in the form of a name-value pair separated by semi-colons.')    
+}
