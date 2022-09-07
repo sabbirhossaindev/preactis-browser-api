b7e7a15407e3589b60a8a718a2837f47e099c980
sabbir hossain <mdsabbir477470@gmail.com>
2022-09-07 17:39:05 +0600

    promet

---
+++ prompt.js
@@ -0,0 +1,13 @@
+const getUserInfo = () => {
+
+    const decision = confirm('Are you coming pricing')
+    console.log(decision);
+    if (decision === true) {
+        alert('mama tk da 200 tk');
+        prompt('mama tk da 200 tk');
+    }
+    else {
+        console.log('dura giya mor')
+    }
+
+}
