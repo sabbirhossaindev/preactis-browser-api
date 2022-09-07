92dcb07d7d62d67f0a2eb960a0c04d90bfae9f22
sabbir hossain <mdsabbir477470@gmail.com>
2022-09-07 18:58:18 +0600

    window location done

---
+++ windowLocation.js
@@ -0,0 +1,9 @@
+// window.location('file:///C:/projects/Browser-api/preactis-browser-api/imdex.html');
+// if (window.location == true) {
+//     alert('ok')
+// }
+const windowLocation = () => {
+    console.log(location);
+    const my = windowLocation.href = "file:///C:/projects/Browser-api/preactis-browser-api/imdex.html";
+    alert(my)
+}
